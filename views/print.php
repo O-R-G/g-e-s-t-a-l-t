1e41f53ff02591168cd19c763dd64b7afbac9d7a
Reinfurt <reinfurt@o-r-g.com>
2022-08-29 19:48:27 +0000

    add all text on the site to output from views/print from w-w-w template commit c152e22f389216956598d0f8f6c0d6a050b060ff

--- views/print.php
+++ views/print.php
@@ -1,31 +1,40 @@
 <section id="main">
     <div id='content' class='make'>
         <div id='columns'><?
-
-            // output meta record * (no begin date)
+            // output about meta record (*)
             foreach($nav as $n) {
                 $body = $n['o']['body'];
                 $date =  $n['o']['begin'];
-                if (!$date) {
+                $url =  $n['o']['url'];
+                if ($url == 'about') {
                     ?><div id='about' class='mono' ><?
                         echo $body;
-                    ?></div><?
+                    ?></div>
+                    <div class='skip-column'>
+                        &nbsp;
+                    </div><?
                 }
             }
-
             // output regular records (begin dates)
             foreach($nav as $n) {
                 $name = $n['o']['name1'];
                 $deck = $n['o']['deck'];
                 $notes = $n['o']['notes'];
                 $date =  $n['o']['begin'];
+                $body =  $n['o']['body'];
+                $find = '/<div><br><\/div>/';
+                $replace = '';
+                $body = preg_replace($find, $replace, $body);
                 if ($date) {
-                    ?><div id='notes' class='mono'><?
+                    ?><div id='notes' class='mono columns'><?
                         echo date("F j, Y", strtotime($date));
                         echo '<br/>';
                         echo $deck;
                         echo '<br/><br/>';
                         echo $notes;
+                    ?></div>
+                    <div id='body' class=''><?
+                        echo $body;
                     ?></div><?
                 }
             }
